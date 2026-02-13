cb4a152aab0a4454005d5ca31be01e7472c9da5e
Ela-El-maker <feloela101@gmail.com>
2026-02-13 21:45:22 +0300

    Update AlarmRepository interface in core-domain

--- core-domain/src/main/kotlin/com/spazoodle/guardian/domain/repository/AlarmRepository.kt
+++ core-domain/src/main/kotlin/com/spazoodle/guardian/domain/repository/AlarmRepository.kt
@@ -5,6 +5,7 @@ import com.spazoodle.guardian.domain.model.Alarm
 interface AlarmRepository {
     suspend fun upsert(alarm: Alarm)
     suspend fun getEnabledAlarms(): List<Alarm>
+    suspend fun getUpcomingAlarms(fromUtcMillis: Long, limit: Int): List<Alarm>
     suspend fun getById(alarmId: Long): Alarm?
     suspend fun setEnabled(alarmId: Long, enabled: Boolean)
 }
